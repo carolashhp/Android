3108973c7b555f547a43848113ad79b749dde779
Carolina Luís <carolina0luis@gmail.com>
2015-04-15 11:29:23 +0100

    versao 2

--- MyFirstApplication/app/src/main/java/android/flag/pt/challenge_it/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/android/flag/pt/challenge_it/myfirstapplication/MainActivity.java
@@ -9,49 +9,86 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener
+{
+    private TextView txtCounter = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // Always call the method from the super class!!!
         super.onCreate(savedInstanceState);
+
+        // This line "connects" the layout with this activity.
         setContentView(R.layout.activity_main);
 
-        final TextView txtCounter = (TextView) findViewById(R.id.txtCounter);
+        // Get the text view for update the integer value.
+        txtCounter = (TextView)findViewById(R.id.txtCounter);
         txtCounter.setText("0");
 
+        // Get the buttons and set the click listener.
+        // In this case, the listener is the "this", so this Activity class
+        // have to implements the interface View.OnClickListener.
+        Button btnIncr = (Button)findViewById(R.id.btnIncr);
+        Button btnDecr = (Button)findViewById(R.id.btnDecr);
+        btnIncr.setOnClickListener(this);
+        btnDecr.setOnClickListener(this);
 
-        Button btnIncr = (Button) findViewById(R.id.btnIncr);
-        //btnIncr.setOnClickListener(this);
+        // The next code demonstrates the usage of the anonymous class for define
+        // the click listeners for the buttons, instead of use the Activity itself.
+        /*
         btnIncr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int current = Integer.parseInt(txtCounter.getText().toString());
-                txtCounter.setText((current + 1 + ""));
+                txtCounter.setText((current + 1) + "");
             }
         });
-        Button btnDecr = (Button) findViewById(R.id.btnDecr);
-        //btnDecr.setOnClickListener(this);
         btnDecr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int current = Integer.parseInt (txtCounter.getText().toString());
-                txtCounter.setText((current - 1 + ""));
+                int current = Integer.parseInt(txtCounter.getText().toString());
+                txtCounter.setText((current - 1) + "");
             }
         });
-
+        */
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    @Override
+    public void onClick(View v)
+    {
+        // Verify which the button was clicked.
+        if(v.getId() == R.id.btnIncr)
+        {
+            int current = Integer.parseInt(txtCounter.getText().toString());
+            txtCounter.setText((current + 1) + "");
+        }
+
+        if(v.getId() == R.id.btnDecr)
+        {
+            int current = Integer.parseInt(txtCounter.getText().toString());
+            txtCounter.setText((current - 1) + "");
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putString("COUNTER_VALUE", txtCounter.getText().toString());
+
+    }
 
     @Override
-    public void onClick(View v) {
-
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        txtCounter.setText(savedInstanceState.getString("COUNTER_VALUE"));
     }
 }
